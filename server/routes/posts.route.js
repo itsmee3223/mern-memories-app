8cb35d493e26640989d753f9c140557e44c0e1d0
Ramanda Ajisaka Asyraf <tamvam.pemberani@gmail.com>
2022-02-16 05:51:03 +0700

    Delete server directory

--- server/routes/posts.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const postController = require('../controllers/post.controller')
-
-const routes = express.Router()
-
-routes.get(, postController.getPost)
-routes.post('/', postController.createPost)
-
-module.exports = routes
